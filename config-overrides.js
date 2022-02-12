836effe856169a0c43de1eaddbe256a851648186
xcsoft <email@xcsoft.top>
2022-02-12 14:19:21 +0800

    try customize-cra

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+const {
+    override,
+    //fixBabelImports,
+    //addWebpackExternals,
+    addWebpackPlugin
+} = require('customize-cra');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = override(
+    addWebpackPlugin(
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+        })
+    ),
+    (config) => {
+        //config.output.publicPath = "https://cdn.timeletters.cn/home_page/"
+        return config;
+    }
+);
