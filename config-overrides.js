614354540878dd82089b08dae5eeda2a5dfb65e1
xcsoft <email@xcsoft.top>
2022-02-12 14:29:40 +0800

    bugFixed

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,21 @@
-import {
+const {
     override,
-    // addWebpackPlugin
-} from 'customize-cra';
-//impoty { BundleAnalyzerPlugin } = from 'webpack-bundle-analyzer'
+    //fixBabelImports,
+    //addWebpackExternals,
+    //addWebpackPlugin
+} = require('customize-cra');
+//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = override(
-    /*
-        addWebpackPlugin(
-            new BundleAnalyzerPlugin({
-                analyzerMode: 'static',
-            })
-        ),
-        (config) => {
-            config.output.publicPath = "https://cdn.timeletters.cn/home_page/"
-            return config;
-    }
-    */
+    /* 
+    addWebpackPlugin(
+         new BundleAnalyzerPlugin({
+             analyzerMode: 'static',
+         })
+     ),
+     (config) => {
+         config.output.publicPath = "https://cdn.timeletters.cn/home_page/"
+         return config;
+     }
+     */
 );
